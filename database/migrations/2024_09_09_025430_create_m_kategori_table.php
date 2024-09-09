5ec1b79557906350e7fbc9c3f91ced5ed9858475
alyssatiarra <tiarraalyssa@gmail.com>
2024-09-09 11:47:56 +0700

    JS3 - Praktikum 2.2

--- database/migrations/2024_09_09_025430_create_m_kategori_table.php
+++ database/migrations/2024_09_09_025430_create_m_kategori_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
+            $table->id('kategori_id')->autoIncrement();
             $table->timestamps();
         });
     }
--- database/migrations/2024_09_09_025452_create_m_supplier_table.php
+++ database/migrations/2024_09_09_025452_create_m_supplier_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_supplier', function (Blueprint $table) {
-            $table->id();
+            $table->id('supplier_id')->autoIncrement();
             $table->timestamps();
         });
     }
