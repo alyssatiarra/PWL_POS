a7f9cfda94a6337d3619367b3befaa00cd0b7ba3
alyssatiarra <tiarraalyssa@gmail.com>
2024-09-22 22:29:16 +0700

    JS 4 - Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,13 +19,36 @@ class UserController extends Controller
         // UserModel::insert($data);
 
         //tambah data dengan Eloquent
-        $data = [
-            'nama' => 'Pelanggan pertama',
-        ];
-        UserModel::where('username', 'customer 1')->update($data); //update data user
+        // $data = [
+        //     'nama' => 'Pelanggan pertama',
+        // ];
+        // UserModel::where('username', 'customer 1')->update($data); //update data user
 
-        // coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data m_user
+        // // coba akses model UserModel
+        // $user = UserModel::all(); //ambil semua data m_user
+        // return view('user', ['data' => $user]);
+
+        // JOBSHEET 4
+        // no 2
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')   
+        // ];
+        // UserModel::create($data);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // no 5
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
