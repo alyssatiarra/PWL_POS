74389dd1788a7f816a030378bf4aba1f34ac2e33
alyssatiarra <tiarraalyssa@gmail.com>
2024-09-23 11:05:58 +0700

    JS 4 - Praktikum 2.6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -50,7 +50,7 @@
         </table> --}}
 
         {{-- prak 2.4 --}}
-        <table border="1" cellpadding="2" cellspacing="0">
+        {{-- <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <td>ID</td>
                 <td>Username</td>
@@ -63,5 +63,31 @@
                 <td> {{$data->nama}} </td>
                 <td> {{$data->level_id}} </td>
             </tr>
+        </table> --}}
+
+        {{-- prak 2.6 --}}
+        <h1>Data User</h1>
+        <a href="{{ url('/user/tambah') }}">+ Tambah User</a>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+                <td>Aksi</td>
+            </tr>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>
+                    <a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> | 
+                    <a href="{{ url('/user/hapus/' . $d->user_id) }}">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
+        </table>
 </body>
 </html>
