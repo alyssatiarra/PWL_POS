f71fcbb927bad3ae99d7933f9fd453891ff96550
alyssatiarra <tiarraalyssa@gmail.com>
2024-09-30 12:03:36 +0700

    JS 6 - Revisi Prak 1

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -17,20 +17,20 @@
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
         <div class="row">
-            <div class="col-md-12">
-                <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Filter:</label>
-                    <div class="col-3">
-                        <select class="form-control" id="level_id" name="level_id" required>
-                            <option value="">- Semua -</option>
-                            @foreach ($level as $item)
-                                <option value="{{$item->level_id}}">{{$item->level_nama}}</option>
-                            @endforeach
-                        </select>
-                        <small class="form-text text-muted">Level Pengguna</small>
-                    </div>
-                </div>
+          <div class="col-md-12">
+            <div class="form-group row">
+              <label class="col-1 control-label col-form-label">Filter:</label>
+              <div class="col-3">
+                <select class="form-control" id="level_id" required>
+                  <option value="">-- Semua --</option>
+                  @foreach ($level as $item)
+                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                  @endforeach
+                </select>
+                <small class="form-text text-muted">Level Pengguna</small>
+              </div>
             </div>
+          </div>
         </div>
       <table class="table table-bordered table-striped table-hover table-sm" id="table_user"> 
         <thead> 
@@ -54,7 +54,7 @@
       var dataUser;
       $(document).ready(function() { 
         dataUser = $('#table_user').DataTable({
-          // serverSide: true, jika ingin menggunakan server side processing 
+          //serverSide: true, jika ingin menggunakan server side processing 
           serverSide: true,      
           ajax: { 
               "url": "{{ url('user/list') }}", 
@@ -98,7 +98,7 @@
           ] 
       }); 
       $('#level_id').on('change', function(){
-        dataUser.ajax.reaload();
+        dataUser.ajax.reload();
       });
     }); 
   </script> 
