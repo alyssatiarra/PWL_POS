729bed47bdb639c88733bc5abe7c7c944b194b18
alyssatiarra <tiarraalyssa@gmail.com>
2024-10-17 22:55:20 +0700

    JS 8 - Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -119,5 +119,6 @@ Route::middleware(['auth', 'authorize:ADM, MNG'])->group(function () {
         Route::get('/import', [BarangController::class, 'import']); //uppload excel
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']); //ajax import excel
         Route::get('/export_excel', [BarangController::class, 'export_excel']);
+        Route::get('/export_pdf', [BarangController::class, 'export_pdf']);
     });
 });
