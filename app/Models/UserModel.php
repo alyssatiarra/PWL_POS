ce94aaec3b69117093c9775a67d79d8d03d7864c
alyssatiarra <tiarraalyssa@gmail.com>
2024-10-28 09:49:41 +0700

    JS 10 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 // class UserModel extends Model
 // {
@@ -27,26 +28,41 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 //     }
 // }
 
-class UserModel extends Authenticatable{
+class UserModel extends Authenticatable implements JWTSubject
+{
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'file_profil', 'created_at', 'update_at'];
     protected $casts = ['password' => 'hashed'];
 
-    public function level(): BelongsTo{
-        return $this -> belongsTo(LevelModel::class, 'level_id');
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id');
     }
 
-    public function getRoleName(): string{
+    public function getRoleName(): string
+    {
         return $this->level->level_nama;
     }
 
-    public function hasRole($role): bool{
-        return $this ->level->level_kode == $role;
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
     }
 
-    public function getRole(){
+    public function getRole()
+    {
         return $this->level->level_kode;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
